14e1e517eed19d78a88192f8b770a79eccf9b523
Snehasish <pathaksnehasish472@gmail.com>
2025-07-06 11:12:17 +0530

    Configuring Cypress and testing on Live Server

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,5 +1,5 @@
-describe('My First Test', () => {
-  it('Does not do much!', () => {
-    expect(true).to.equal(true)
+describe('The Home Page', () => {
+  it('successfully loads', () => {
+    cy.visit('/')
   })
 })
